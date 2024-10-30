dcd06f7b2be41804b702382538bfcff45f0b041f
Sahil-047 <sahil_gcse2022@msit.edu.in>
2024-10-30 18:23:26 +0530

    Schema changes 1.0

---
+++ backend/Models/Member.model.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+
+const memberSchema = new mongoose.Schema({
+    domain: [String],
+    socials: {
+        linkedin: String,
+        instagram: String,
+        twitter: String,
+        //add kar lo aur chahiye to
+    },
+    yearOfJoining: Number,
+    yearOfPassing: Number,
+    stream: String,
+    universityRollNumber: Number,
+    avatarUrl: String,
+    skills:[String],
+    events:[{type: mongoose.Schema.Types.ObjectId, ref:'Event'}],
+    projects:[{type: mongoose.Schema.Types.ObjectId, ref:'Project'}],
+    attendance:[{type: mongoose.Schema.Types.ObjectId, ref:'Attendance'}],
+    
+},{timestamps: true})
+
+export const UserModel = mongoose.model('Member', memberSchema)
